eb83be56388d8a70d3964e398585eaeda46266b3
joneseyit <joneseyit@protonmail.com>
2018-10-25 19:59:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.to_s.capitalize}:"
       value.each do |holiday, supplies|
-      #holiday = format_phrase(holiday)
+      holiday = format_phrase(holiday)
       puts "  #{formatphrase(holiday)}: #{supplies.join(", ")}"
     end
   end
